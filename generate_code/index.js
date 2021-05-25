b27aba56ffb6a23dae5b3f0b728be66f910f74b7
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-05-26 00:17:37 +0300

    Generate Code Task Added

---
+++ generate_code/index.js
@@ -0,0 +1,14 @@
+
+const { default: generate } = require('@babel/generator')
+const http = require('http')
+
+const URL = 'http://kodaktor.ru/j/_ast/7f7e0'
+
+http.get(URL, (r, b = '') => r
+.on('data', d => b += d.toString())
+.on('end', () => {
+const ast = JSON.parse(b)
+const { code } = generate(ast)
+console.log(code)
+})
+)
